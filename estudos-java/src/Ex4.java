0e2f6ecfbc065e9851031e91845236a97bbbfc2f
Bianca Almeida <151306147+biancalmds@users.noreply.github.com>
2025-06-02 19:33:07 -0300

    finalizado o exercício 4

---
+++ estudos-java/src/Ex4.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex4 {
+	
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		int nFuncionario, horasTrabalhadas;
+		double valorHora, salario;
+		
+		System.out.print("Digite o número do funcionário: ");
+		nFuncionario = sc.nextInt();
+		System.out.print("Digite a quantidade de horas trabalhadas do funcionário: ");
+		horasTrabalhadas = sc.nextInt();
+		System.out.print("Digite o valor que ele recebe por hora: ");
+		valorHora = sc.nextDouble();
+		salario = valorHora * horasTrabalhadas;
+		
+		System.out.printf("\nNúmero do funcionário: %d\n", nFuncionario);
+		System.out.printf("Salário do funcionário: R$ %.2f\n", salario);
+		sc.close();
+	}
+
+}
