3766b4cc84a893138400155a12d8bde6f55fa8e7
Bianca Almeida <151306147+biancalmds@users.noreply.github.com>
2025-06-02 18:54:50 -0300

    primeiro commit

---
+++ estudos-java/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1, num2, soma;
+		System.out.println("Digite o 1° número: ");
+		num1 = sc.nextInt();
+		System.out.println("Digite o 1° número: ");
+		num2 = sc.nextInt();
+		soma = num1 + num2;
+		System.out.println("SOMA = " + soma);
+		sc.close();
+	}
+}
