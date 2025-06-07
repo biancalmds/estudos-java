88f54b1390661eb72c7669babf2a0e84d6eabd18
Bianca Almeida <151306147+biancalmds@users.noreply.github.com>
2025-06-07 19:09:01 -0300

    finalizado o exercício 6

---
+++ estudos-java/src/estrutura_condicional/Ex6.java
@@ -0,0 +1,30 @@
+package estrutura_condicional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Ex6 {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		double num;
+		
+		System.out.println("Digite um número de 0 a 100:");
+		num = sc.nextDouble();
+		
+		if (num < 0) {
+			System.out.println("Fora de intervalo");
+		} else if (num <=25) {
+			System.out.println("Intervalo [0,25]");
+		} else if (num <= 50) {
+			System.out.println("Intervalo (25,50]");
+		} else if (num <= 75) {
+			System.out.println("Intervalo  (50,75]");
+		} else if (num <= 100) {
+			System.out.println("Intervalo (75,100]");
+		} else {
+			System.out.println("Fora do intervalo");
+		}
+		sc.close();
+	}
+}
